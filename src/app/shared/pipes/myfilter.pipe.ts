3554d54e3206cb63e1272a04cc2f6670a363baf2
Dmitry Lisitsyn <Dimchik.lis@mail.ru>
2020-05-20 11:31:14 +0300

    second

--- src/app/shared/pipes/myfilter.pipe.ts
+++ src/app/shared/pipes/myfilter.pipe.ts
@@ -15,25 +15,26 @@ export class MyfilterPipe implements PipeTransform {
 
   }
 
-  transform(items: any[], searchName: string, searchSurname: string): any[] {
+  transform(items: any[], searchName: string): any[] {
 
     //this.container = items;
-    
+    let _fullname = searchName.split(" ");
+
     
       let filteredValues = items;
 
-    if(searchName != "" && searchName != null ) {
+    if(_fullname[0] != "" && _fullname[0] != null ) {
 
       filteredValues = items.filter(
-        (item) => item.name.indexOf(searchName) !== -1
+        (item) => item.name.indexOf(_fullname[0]) !== -1
 
        );
 
       }
-    if(searchSurname != "" && searchSurname != null ) {
+    if(_fullname[1] != "" && _fullname[1] != null ) {
 
       filteredValues = items.filter(
-        (item) => item.surname.indexOf(searchSurname) !== -1
+        (item) => item.surname.indexOf(_fullname[1]) !== -1
 
        );
 
